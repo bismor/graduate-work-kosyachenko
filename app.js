9c0ba956653ea8895c9dca11ad1768c2aaaef4a5
Vladislav Kosyachenko <cwat2011@yandex.ru>
2023-05-17 13:38:03 +0300

    аутентификацию и авторизацию

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { celebrate, Joi } = require('celebrate');
 const auth = require('./middlewares/auth');
-// const { celebrate, Joi } = require('celebrate');
+const { createUser, login } = require('./controllers/users');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,6 +12,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().regex(/^\S+@\S+\.\S+$/),
+    password: Joi.string().required(),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/),
+    email: Joi.string().required().regex(/^\S+@\S+\.\S+$/),
+    password: Joi.string().required(),
+  }),
+}), createUser);
 
 app.use('/movies', auth, require('./routes/movies'));
 app.use('/users', auth, require('./routes/users'));
