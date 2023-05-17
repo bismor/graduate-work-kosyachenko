2cd313f20a353b6c1aab45f17c4bc077aee437cd
Vladislav Kosyachenko <cwat2011@yandex.ru>
2023-05-17 13:18:43 +0300

    auth movies

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const auth = require('./middlewares/auth');
 // const { celebrate, Joi } = require('celebrate');
 
 const app = express();
@@ -10,5 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
+
+app.use('/movies', auth, require('./routes/movies'));
 
 app.listen(3000);
--- routes/movies.js
+++ routes/movies.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 // const { celebrate, Joi } = require('celebrate');
 const { getMovies, createMovies, deleteMoviesById } = require('../controllers/movies');
-const auth = require('../middlewares/auth');
 
-router.get('/', auth, getMovies);
-router.post('/', auth, createMovies);
-router.delete('/_id', auth, deleteMoviesById);
+router.get('/', getMovies);
+router.post('/', createMovies);
+router.delete('/_id', deleteMoviesById);
