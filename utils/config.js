a274e0c57af6c81776a32b95089e2d9bff1ff1a9
Vladislav Kosyachenko <cwat2011@yandex.ru>
2023-05-26 10:37:09 +0300

    adress local server

--- utils/config.js
+++ utils/config.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   PORT: 3000,
-  MONGO_BD_URL: (process.env.NODE_ENV !== 'production') ? 'mongodb://localhost:27017/movies-explorerdb' : process.env.MONGO_BD_URL,
+  MONGO_BD_URL: (process.env.NODE_ENV !== 'production') ? 'mongodb://127.0.0.1:27017/filmsbd' : process.env.MONGO_BD_URL,
   JWT_SECRET: (process.env.NODE_ENV !== 'production') ? 'JWT_SECRET' : process.env.JWT_SECRET,
 };
