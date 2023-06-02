acc4de09b9f9a2c8c60b03aca038876ebffa5b62
Vladislav Kosyachenko <cwat2011@yandex.ru>
2023-06-02 19:35:09 +0300

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,8 +3,15 @@ import './Footer.css';
 export default function Footer() {
 
   return (
-    <footer>
-
+    <footer className='footer'>
+      <h2 className='footer__title'>Яндекс.Практикум х BeatFilm</h2>
+      <div className='footer__container'>
+        <p className='footer__text'>&copy; 2023</p>
+        <div className='footer__container-link'>
+            <a href='https://practicum.yandex.ru/' title='Практикум' className='footer__link'>Яндекс.Практикум</a>
+            <a href='https://github.com/Hotess' title='Github' className='footer__link'>Github</a>
+        </div>
+      </div>
     </footer>
   );
 }
