9a153274c6cf786a617a503de62d569ec26cb38c
Vladislav Kosyachenko <cwat2011@yandex.ru>
2023-06-02 23:03:43 +0300

    promo гибкий

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -5,11 +5,11 @@ import landingLlogo from "../../images/landing-logo.png"
 export default function Promo() {
   return (
     <section className='promo'>
+      <img src={landingLlogo} className='promo__logo' alt='Логотип Земли'/>
       <div className='promo__description'>
         <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
         <p className='promo__subtitle'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
       </div>
-      <img src={landingLlogo} className='promo__logo' alt='Логотип Земли'/>
       <Link to="#AboutProject" className='promo__link' title='Узнать больше'>Узнать больше</Link>
 
     </section>
