83509c4f46b79788eec08b1cd63c2dced41a52a8
Vladislav Kosyachenko <cwat2011@yandex.ru>
2023-06-02 11:48:02 +0300

    О проекте

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -1,17 +1,23 @@
-import './AboutProject.css'
+import "./AboutProject.css";
 
 export default function AboutProject() {
-  return(
-    <section className='aboutproject'>
-      <h2 className='aboutproject__title'>Технологии</h2>
-      <div className='aboutproject_about'>
-        <div className='aboutproject__bio'>
+  return (
+    <section className="aboutproject">
+      <h2 className="aboutproject__title">Студент</h2>
+      <div className="aboutproject_about">
+        <div className="aboutproject__bio">
           <h3>Владислав</h3>
           <h4>Фронтенд-разработчик, 25 лет</h4>
-          <p> биография</p>
+          <p>
+            Я родился и живу в Королеве. Закончил МЭИ, факультет
+            электроэнергетики и электротехники.
+          </p>
+          <div className="aboutproject__git">
+          <a href="https://github.com/bismor" title='GitHub' className='aboutMe__link'>GitHub</a>
+          </div>
         </div>
+        <div className="aboutproject__image"></div>
       </div>
-
     </section>
-  )
+  );
 }
