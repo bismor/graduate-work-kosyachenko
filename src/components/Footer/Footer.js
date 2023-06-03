31c95ad3b4c536f1d5bd7e44a2634eedd15403bc
Vladislav Kosyachenko <cwat2011@yandex.ru>
2023-06-03 14:40:59 +0300

    footer rem

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
 
   return (
     <footer className='footer'>
-      <h2 className='footer__title'>Яндекс.Практикум х BeatFilm</h2>
+      <h2 className='footer__title'>Учебный проект Яндекс.Практикум х BeatFilm</h2>
       <div className='footer__container'>
         <p className='footer__text'>&copy; 2023</p>
         <div className='footer__container-link'>
