d2fc08927233ffd89ef903d4181477cf3dde0d43
Vladislav Kosyachenko <cwat2011@yandex.ru>
2023-06-03 23:02:13 +0300

    login

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom'
 import Main from './Main/Main'
 import Register from './Register/Register';
+import Login from './Login/Login'
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
           }
         />
         <Route path='/signup'element={<Register />}/>
+        <Route path='/signin'element={<Login />}/>
       </Routes>
     </div>
   );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,34 @@
 import './Login.css';
+import Logo from "../ui/Logo/Logo"
 import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
-    <>
-    <Link to='/login' className='login'>
-      
-    </Link>
-    </>
+    <div className="login">
+      <Logo/>
+      <h1 className="login__name">Рады видеть!</h1>
+      <form className="login__form">
+        <section className="login__section">
+          <label className="login__label">E-mail
+            <input
+              name="email"
+              type="email"
+              className="login__input"
+            ></input>
+          </label>
+          <label className="login__label">Пароль
+            <input
+              name="password"
+              type="password"
+              className="login__input"
+            ></input>
+          </label>
+        </section>
+        <button className="login__submit">Войти</button>
+        <p className="login__text">
+          Ещё не зарегистрированы? <Link to="/signup" className="login__link">Регистрация</Link>
+        </p>
+      </form>
+    </div>
   )
 }
