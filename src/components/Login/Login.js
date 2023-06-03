8b70e8eec0a85521081b8e99d134f715cb7faf75
Vladislav Kosyachenko <cwat2011@yandex.ru>
2023-06-04 01:35:54 +0300

    login profile

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,15 +2,22 @@ import './Login.css';
 import Logo from "../ui/Logo/Logo"
 import { Link, useNavigate } from "react-router-dom";
 
-export default function Login() {
+export default function Login({setloggedIn}) {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setloggedIn(true);
+    navigate("/profile", { replace: true });
+  };
+
+
 
   return (
     <div className="login">
       <Logo/>
       <h1 className="login__name">Рады видеть!</h1>
-      <form className="login__form" onSubmit={() => { navigate("/profile", { replace: true })}}>
+      <form className="login__form" onSubmit={handleSubmit}>
         <section className="login__section">
           <label className="login__label">E-mail
             <input
