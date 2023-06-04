fa467a1430fd7cf9c0d5f9069320149696b0b4ab
Vladislav Kosyachenko <cwat2011@yandex.ru>
2023-06-04 09:43:19 +0300

    not found

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,12 @@ import Main from "./Main/Main";
 import Register from "./Register/Register";
 import Login from "./Login/Login";
 import Profile from "./Profile/Profile";
+import NotFound from "./NotFound/NotFound";
 
 export default function App() {
   const [loggedIn, setloggedIn] = useState(false);
+  const [isHamburger, setIsHamburger] = useState(false);
+
   return (
     <div className="page">
       <Routes>
@@ -15,6 +18,7 @@ export default function App() {
         <Route path="/signup" element={<Register setloggedIn={setloggedIn}/>} />
         <Route path="/signin" element={<Login setloggedIn={setloggedIn} />} />
         <Route path="/profile" element={<Profile setloggedIn={setloggedIn} />} />
+        <Route path="/*" element={<NotFound />}/>
       </Routes>
     </div>
   );
