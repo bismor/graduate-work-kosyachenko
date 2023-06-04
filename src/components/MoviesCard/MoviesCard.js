7615b41905c97995b74374743cd4f5d5ef6a260c
Vladislav Kosyachenko <cwat2011@yandex.ru>
2023-06-04 21:51:58 +0300

    fix

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -12,10 +12,11 @@ export default function MoviesCard() {
             <a href="https://youtu.be/0bWUPgK-lt0" title="33 слова о дизайне" className='movies__link'>
                 <h2 className='movies__heading'>33 слова о дизайне</h2>
             </a>
-                <button
-                    className={`movies__button movies__button_saved`}
-                />
+            <button
+                className={`movies__button movies__button_saved`}
+            />
         </div>
+        <p className='movies__duration'>1ч42мс</p>
       </article>
       <article className='movies__cell'>
         <a href="https://youtu.be/0bWUPgK-lt0" title="33 слова о дизайне" className='movies__link'>
@@ -29,6 +30,7 @@ export default function MoviesCard() {
                     className={`movies__button movies__button_saved`}
                 />
         </div>
+        <p className='movies__duration'>1ч42мс</p>
       </article>
     </>
   )
