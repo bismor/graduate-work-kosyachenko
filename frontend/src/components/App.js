25be8d4371dc7a23a7f8015eb430110897f92336
Vladislav Kosyachenko <cwat2011@yandex.ru>
2023-06-09 18:06:11 +0300

    CurrentUserContext.Provider

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -8,8 +8,9 @@ import Profile from "./Profile/Profile";
 import NotFound from "./NotFound/NotFound";
 import Movies from "./Movies/Movies";
 import SavedMovies from "./SavedMovies/SavedMovies";
-import api from "../utils/Api"
+import api from "../utils/Api";
 import auth from "../utils/Auth";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 export default function App() {
   const [currentUser, setCurrentUser] = useState(null);
@@ -83,54 +84,62 @@ export default function App() {
   }
 
   return (
-    <div className="page">
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Main
-              loggedIn={loggedIn}
-              isHamburger={isHamburger}
-              setIsHamburger={setIsHamburger}
-              onHandleHamburger={onHandleHamburger}
-            />
-          }
-        />
-        <Route
-          path="/signup"
-          element={<Register setloggedIn={setloggedIn} />}
-        />
-        <Route
-          path="/signin"
-          element={<Login signIn={signIn} />}
-        />
-        <Route
-          path="/profile"
-          element={
-            <Profile
-              loggedIn={loggedIn}
-              onLogout={onLogout}
-              isHamburger={isHamburger}
-              setIsHamburger={setIsHamburger}
-              onHandleHamburger={onHandleHamburger}
-            />
-          }
-        />
-        <Route path="/*" element={<NotFound />} />
-        <Route path="/movies" element={<Movies
-          loggedIn={loggedIn}
-          isHamburger={isHamburger}
-          setIsHamburger={setIsHamburger}
-          onHandleHamburger={onHandleHamburger}
-        />}/>
-        <Route path='/saved-movies' element={<SavedMovies
-          loggedIn={loggedIn}
-          isHamburger={isHamburger}
-          setIsHamburger={setIsHamburger}
-          onHandleHamburger={onHandleHamburger}
-          />}
-        />
-      </Routes>
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="page">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Main
+                loggedIn={loggedIn}
+                isHamburger={isHamburger}
+                setIsHamburger={setIsHamburger}
+                onHandleHamburger={onHandleHamburger}
+              />
+            }
+          />
+          <Route
+            path="/signup"
+            element={<Register setloggedIn={setloggedIn} />}
+          />
+          <Route path="/signin" element={<Login signIn={signIn} />} />
+          <Route
+            path="/profile"
+            element={
+              <Profile
+                loggedIn={loggedIn}
+                onLogout={onLogout}
+                isHamburger={isHamburger}
+                setIsHamburger={setIsHamburger}
+                onHandleHamburger={onHandleHamburger}
+              />
+            }
+          />
+          <Route path="/*" element={<NotFound />} />
+          <Route
+            path="/movies"
+            element={
+              <Movies
+                loggedIn={loggedIn}
+                isHamburger={isHamburger}
+                setIsHamburger={setIsHamburger}
+                onHandleHamburger={onHandleHamburger}
+              />
+            }
+          />
+          <Route
+            path="/saved-movies"
+            element={
+              <SavedMovies
+                loggedIn={loggedIn}
+                isHamburger={isHamburger}
+                setIsHamburger={setIsHamburger}
+                onHandleHamburger={onHandleHamburger}
+              />
+            }
+          />
+        </Routes>
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
