85daa04ff0de24471219fc68446f0e6a430165e6
Vladislav Kosyachenko <cwat2011@yandex.ru>
2023-06-13 23:01:12 +0300

    поиск

--- frontend/src/components/SearchForm/SearchForm.js
+++ frontend/src/components/SearchForm/SearchForm.js
@@ -1,32 +1,53 @@
-import './SearchForm.css'
+import "./SearchForm.css";
+import React, { useState, useEffect } from "react";
 
-export default function SearchForm() {
+export default function SearchForm({
+  isShortMovies,
+  onSearchSubmit,
+  onSearchShortMovies,
+}) {
+  const [searchedMovies, setSearchedMovies] = useState("");
+  function handleSearchMovie(event) {
+    event.preventDefault();
+
+    onSearchSubmit(searchedMovies);
+  }
+
+  useEffect(() => {
+    onSearchSubmit(searchedMovies);
+  }, [isShortMovies]);
 
   return (
-    <form className='form-search'>
-      <div className='form-search__container'>
-          <input
-              type='search'
-              placeholder='Фильм'
-              className='form-search__input'
-          />
-          <button
-              type='button'
-              aria-label='Поиск'
-              className='form-search__magnifier'
-          />
+    <form className="form-search">
+      <div className="form-search__container">
+        <input
+          type="search"
+          placeholder="Поиск"
+          className="form-search__input"
+          onInput={(event) => setSearchedMovies(event.target.value)}
+        />
+        <button
+          type="button"
+          aria-label="Поиск"
+          className="form-search__magnifier"
+          onClick={(event) => handleSearchMovie(event)}
+        />
       </div>
-      <div className='form-search__toggle'>
-          <span className='form-search__text'>Короткометражки</span>
-          <button
-              type='button'
-              aria-label='Искать короткометражки'
-              className='toggle'
-          >
-              <span className='toggle__turn'>
-              </span>
-          </button>
+      <div className="form-search__toggle">
+        <span className="form-search__text">Короткометражки</span>
+        <button
+          type="button"
+          aria-label="Искать короткометражки"
+          className={`toggle ${isShortMovies ? "toggle_active" : ""} `}
+          onClick={onSearchShortMovies}
+        >
+          <span
+            className={`toggle__turn ${
+              isShortMovies ? "toggle__turn_active" : ""
+            }`}
+          ></span>
+        </button>
       </div>
     </form>
-  )
+  );
 }
